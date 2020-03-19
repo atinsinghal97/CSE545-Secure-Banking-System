e9c238c00362efaf7d7d1cfa26045563e1b8ba13
Raj <buddhdev.raj34@gmail.com>
2020-03-18 19:31:16 -0700

    started transaction implementation

--- src/main/java/web/Tier1DashboardController.java
+++ src/main/java/web/Tier1DashboardController.java
@@ -13,37 +13,31 @@ public class Tier1DashboardController {
 	@RequestMapping(value = "/Tier1Dashboard")
 	public String dashboard(final HttpServletRequest request, HttpSession session, Model model) {
 		return "Tier1Dashboard";
-		
 	}
 	
 	@RequestMapping(value = "/Tier1PendingTransactions")
 	public String pendingTransactions(HttpServletRequest request, HttpSession session, Model model) {
 		return "Tier1PendingTransactions";
-		
 	}
 	
 	@RequestMapping(value = "/IssueCheque")
 	public String issueCheque(HttpServletRequest request, HttpSession session, Model model) {
 		return "IssueCheque";
-		
 	}
 	
 	@RequestMapping(value = "/Tier1DepositMoney")
 	public String depositAmount(HttpServletRequest request, HttpSession session, Model model) {
 		return "Tier1DepositMoney";
-		
 	}
 	
 	@RequestMapping(value = "/Tier1WithdrawMoney")
 	public String withdrawAmount(HttpServletRequest request, HttpSession session, Model model) {
 		return "Tier1WithdrawMoney";
-		
 	}
 
 	@RequestMapping(value = "/Tier1UpdatePassword")
 	public String updatePassword(HttpServletRequest request, HttpSession session, Model model) {
 		return "Tier1UpdatePassword";
-		
 	}
 	
 }
