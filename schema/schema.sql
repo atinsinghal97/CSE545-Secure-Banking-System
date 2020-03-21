2b9444ef9869c6d0025ebfb92132f8e2e92d27bc
kangjianma <46874827+kangjianma@users.noreply.github.com>
2020-03-20 19:02:02 -0700

    Updated schema file

--- schema/schema.sql
+++ schema/schema.sql
@@ -5,24 +5,24 @@ CREATE DATABASE secure_banking_system;
 USE secure_banking_system;
 
 CREATE TABLE `secure_banking_system`.`user` (
-  user_id INT PRIMARY KEY AUTO_INCREMENT,
-  username VARCHAR(255) NOT NULL,
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(60) NOT NULL,
   status INT NOT NULL,
   incorrect_attempts INT DEFAULT 0,
   created_date DATETIME DEFAULT NOW(),
   modified_date DATETIME DEFAULT NOW(),
-  user_type ENUM('tier1', 'tier2', 'admin', 'customer', 'merchant')
+  role VARCHAR(100)
 );
 
 CREATE TABLE `secure_banking_system`.`user_details` (
+  id INT PRIMARY KEY AUTO_INCREMENT,
   user_id INT NOT NULL,
   first_name VARCHAR(255) NOT NULL,
   middle_name VARCHAR(255) DEFAULT NULL,
   last_name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   phone VARCHAR(15) NOT NULL,
-  tier VARCHAR(10),
   address1 VARCHAR(255) NOT NULL,
   address2 VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
@@ -32,86 +32,108 @@ CREATE TABLE `secure_banking_system`.`user_details` (
   ssn VARCHAR(15) NOT NULL UNIQUE,
   question_1 VARCHAR(255) NOT NULL,
   question_2 VARCHAR(255) NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES `secure_banking_system`.`user`(user_id) ON DELETE CASCADE
+  FOREIGN KEY (user_id) REFERENCES `secure_banking_system`.`user`(id) ON DELETE CASCADE
 );
 
 CREATE TABLE `secure_banking_system`.`account` (
-  account_id INT PRIMARY KEY AUTO_INCREMENT,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   user_id INT NOT NULL,
   account_number VARCHAR(255) NOT NULL,
-  account_type ENUM('savings', 'checking', 'credit') NOT NULL,
-  current_balance DECIMAL(10, 5) DEFAULT 0.0,
+  account_type VARCHAR(100) NOT NULL,
+  current_balance DECIMAL(30, 5) DEFAULT 0.0,
   created_date DATETIME NOT NULL DEFAULT NOW(),
-  approval_status BOOLEAN NOT NULL,
+  status BOOLEAN NOT NULL,
   interest DECIMAL(10, 5) DEFAULT 0.0,
   approval_date DATETIME,
-  approver INT,
-  FOREIGN KEY (approver) REFERENCES `secure_banking_system`.`user`(user_id) ON DELETE CASCADE,
-  FOREIGN KEY (user_id) REFERENCES `secure_banking_system`.`user`(user_id) ON DELETE CASCADE
+  FOREIGN KEY (user_id) REFERENCES `secure_banking_system`.`user`(id)
 );
 
 CREATE TABLE `secure_banking_system`.`transaction` (
-  transaction_id INT PRIMARY KEY AUTO_INCREMENT,
-  transaction_type ENUM('cc', 'debit', 'transfer') NOT NULL,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   approval_status BOOLEAN NOT NULL,
   amount DECIMAL(10, 5),
   is_critical_transaction BOOLEAN NOT NULL,
   requested_date DATETIME NOT NULL DEFAULT NOW(),
-  decision_date DATETIME NOT NULL DEFAULT NOW(),
+  decision_date DATETIME DEFAULT NULL,
   from_account INT NOT NULL,
   to_account INT NOT NULL,
-  approver INT NOT NULL, /* final approval which should trigger transfer. Tier 2 can directly approve critical transactions no problem. */
-  level_1_approval BOOLEAN DEFAULT NULL, /* Customer approval */
-  level_2_approval BOOLEAN DEFAULT NULL, /* Tier 2 employee approval */
-  FOREIGN KEY (from_account) REFERENCES `secure_banking_system`.`account`(account_id),
-  FOREIGN KEY (to_account) REFERENCES `secure_banking_system`.`account`(account_id),
-  FOREIGN KEY (approver) REFERENCES `secure_banking_system`.`user`(user_id)
+  transaction_type VARCHAR(100) NOT NULL,
+  request_assigned_to INT DEFAULT NULL,
+  approval_level_required VARCHAR(100) NOT NULL,
+  level_1_approval BOOLEAN DEFAULT NULL,
+  level_2_approval BOOLEAN DEFAULT NULL,
+  approved BOOLEAN DEFAULT NULL, /* Can be approved by merchant or bank employee depending on type of request */
+  FOREIGN KEY (request_assigned_to) REFERENCES `secure_banking_system`.`user`(id),
+  FOREIGN KEY (from_account) REFERENCES `secure_banking_system`.`account`(id),
+  FOREIGN KEY (to_account) REFERENCES `secure_banking_system`.`account`(id)
 );
 
 CREATE TABLE `secure_banking_system`.`appointment` (
-  appointment_id INT PRIMARY KEY AUTO_INCREMENT,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   appointment_user_id INT NOT NULL,
   assigned_to_user_id INT NOT NULL,
   created_date DATETIME NOT NULL DEFAULT NOW(),
   appointment_status VARCHAR(25) NOT NULL,
-  FOREIGN KEY (appointment_user_id) REFERENCES `secure_banking_system`.`user`(user_id),
-  FOREIGN KEY (assigned_to_user_id) REFERENCES `secure_banking_system`.`user`(user_id)
-);
-
-CREATE TABLE `secure_banking_system`.`request` (
-  request_id INT PRIMARY KEY AUTO_INCREMENT,
-  requested_by INT NOT NULL,
-  type_of_request INT NOT NULL, /* request to modify account, request to ??? */
-  request_assigned_to INT DEFAULT NULL,
-  type_of_account VARCHAR(25) NOT NULL,
-  approved BOOLEAN DEFAULT NULL, /* Can be approved by merchant or bank employee depending on type of request */
-  FOREIGN KEY (request_assigned_to) REFERENCES `secure_banking_system`.`user`(user_id),
-  FOREIGN KEY (requested_by) REFERENCES `secure_banking_system`.`user`(user_id)
+  FOREIGN KEY (appointment_user_id) REFERENCES `secure_banking_system`.`user`(id),
+  FOREIGN KEY (assigned_to_user_id) REFERENCES `secure_banking_system`.`user`(id)
 );
 
 CREATE TABLE `secure_banking_system`.`login_history` (
+  id INT PRIMARY KEY AUTO_INCREMENT, 
   user_id INT NOT NULL,
   logged_in DATETIME NOT NULL DEFAULT NOW(),
   logged_out DATETIME DEFAULT NULL,
   ip_address VARCHAR(25) NOT NULL,
   device_type VARCHAR(25) NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES `secure_banking_system`.`user`(user_id)
+  FOREIGN KEY (user_id) REFERENCES `secure_banking_system`.`user`(id)
 );
 
-CREATE VIEW `secure_banking_system`.`customer` AS SELECT * FROM `secure_banking_system`.`user` WHERE user_type = 'customer';
-CREATE VIEW `secure_banking_system`.`merchant` AS SELECT * FROM `secure_banking_system`.`user` WHERE user_type = 'merchant';
-CREATE VIEW `secure_banking_system`.`tier1` AS SELECT * FROM `secure_banking_system`.`user` WHERE user_type = 'tier1';
-CREATE VIEW `secure_banking_system`.`tier2` AS SELECT * FROM `secure_banking_system`.`user` WHERE user_type = 'tier2';
-CREATE VIEW `secure_banking_system`.`admin` AS SELECT * FROM `secure_banking_system`.`user` WHERE user_type = 'admin';
+DELIMITER $$
 
+CREATE PROCEDURE `secure_banking_system`.`create_user_transaction` (
+	IN transfer_type ENUM('cc', 'debit', 'transfer'),
+	IN from_username VARCHAR(255),
+	IN from_account INT,
+	IN to_account INT,
+	IN amount DECIMAL,
+	OUT status INT)
+BEGIN
+	DECLARE total_amount_transferred_today DECIMAL(30, 5) DEFAULT 0;
+	DECLARE user_count INT;
+	DECLARE trigger_user_id INT;
 
-CREATE VIEW `secure_banking_system`.`savings_account` AS SELECT * FROM `secure_banking_system`.`account` WHERE account_type = 'savings';
-CREATE VIEW `secure_banking_system`.`checking_account` AS SELECT * FROM `secure_banking_system`.`account` WHERE account_type = 'checking';
-CREATE VIEW `secure_banking_system`.`credit_account` AS SELECT * FROM `secure_banking_system`.`account` WHERE account_type = 'credit';
+	SELECT IFNULL(SUM(amount), 0) 
+		INTO total_amount_transferred_today 
+		FROM `secure_banking_system`.`transaction` AS t 
+		WHERE t.from_account = from_account
+			AND DATE(t.requested_date) = CURDATE();
 
-CREATE VIEW `secure_banking_system`.`cheque_transaction` AS SELECT * FROM `secure_banking_system`.`transaction` WHERE transaction_type = 'cc';
-CREATE VIEW `secure_banking_system`.`transfer_transaction` AS SELECT * FROM `secure_banking_system`.`transaction` WHERE transaction_type = 'transfer';
-CREATE VIEW `secure_banking_system`.`debit_transaction` AS SELECT * FROM `secure_banking_system`.`transaction` WHERE transaction_type = 'debit';
+	SELECT COUNT(*) INTO user_count
+		FROM `secure_banking_system`.`account` AS a JOIN `secure_banking_system`.`user` AS u ON a.user_id = u.id
+		WHERE (a.id = from_account AND u.username = from_username AND u.status = 1) OR (a.id = to_account AND u.status = 1);
 
-CREATE VIEW `secure_banking_system`.`critical_transaction` AS SELECT * FROM `secure_banking_system`.`transaction` WHERE is_critical_transaction = TRUE;
-CREATE VIEW `secure_banking_system`.`non_critical_transaction` AS SELECT * FROM `secure_banking_system`.`transaction` WHERE is_critical_transaction = FALSE;
+	SELECT id INTO trigger_user_id
+		FROM `secure_banking_system`.`user`
+		WHERE username = from_username;
+
+	IF user_count != 2 THEN
+		SET status = 3;
+	ELSEIF (total_amount_transferred_today + amount > 1000.0) THEN
+		INSERT INTO `secure_banking_system`.`transaction` (transaction_type, approval_status, amount, is_critical_transaction, from_account, to_account)
+			VALUES(transfer_type, FALSE, amount, TRUE, from_account, to_account);
+
+		INSERT INTO `secure_banking_system`.`request` (requested_by, request_id, type_of_request, approval_level_required)
+		  VALUES(trigger_user_id, LAST_INSERT_ID(), "transaction", "tier2");
+		SET status = 1;
+	ELSEIF (SELECT IFNULL(current_balance, 0) FROM `secure_banking_system`.`account` WHERE id = from_account) < amount THEN
+		SET status = 2;
+	ELSE
+		INSERT INTO `secure_banking_system`.`transaction` (transaction_type, approval_status, amount, is_critical_transaction, from_account, to_account)
+			VALUES(transfer_type, FALSE, amount, FALSE, from_account, to_account);
+		INSERT INTO `secure_banking_system`.`request` (requested_by, request_id, type_of_request, approval_level_required)
+		  VALUES(trigger_user_id, LAST_INSERT_ID(), "transaction", "tier1");
+		SET status = 0;
+	END IF;
+END$$
+
+DELIMITER ;
