4b49ac39786d5ba63ac2863f397dfd03217b50f2
Raj <buddhdev.raj34@gmail.com>
2020-03-23 08:13:48 -0700

    removed default account from constants

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -17,7 +17,6 @@ public final class Constants {
     public static final int DEFAULT_TIER1 = 9;
     public static final int DEFAULT_TIER2 = 3;
     public static final int DEFAULT_ADMIN = 4;
-    //public static final String DEFAULT_BANK_ACCOUNT = "100";
     public static final String TRANSFER = "transfer";
     public static final String DEBIT = "debit";
     public static final String CREDIT = "credit";
