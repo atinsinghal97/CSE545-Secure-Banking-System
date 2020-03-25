34dab4af8788c2e6e6d5ea23928367cbc918f8fe
Amitabh Das <amitab.das@outlook.com>
2020-03-25 12:46:00 -0700

    Load database connection before server starts, rather than wait till the first database access.

--- src/main/java/bankApp/application/Application.java
+++ src/main/java/bankApp/application/Application.java
@@ -7,12 +7,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import database.SessionManager;
+
 @ComponentScan(basePackages = {"web", "bankApp.repositories", "model", "security", "communication"})
 @SpringBootApplication
 public class Application {
 	public static ExecutorService executorService;
 
 	public static void main(String[] args) {
+		SessionManager.init();
 		executorService = Executors.newFixedThreadPool(10);
 		SpringApplication.run(Application.class, args);
 	}
