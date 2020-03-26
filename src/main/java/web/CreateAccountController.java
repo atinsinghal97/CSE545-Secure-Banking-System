b19dc08f22e4c1c58e33fb2fee90d20e89e46f21
uttambhat <uttambhat@192.168.0.5>
2020-03-25 23:23:21 -0700

    otp works, create new account and cashiers check are working

--- src/main/java/web/CreateAccountController.java
+++ src/main/java/web/CreateAccountController.java
@@ -1,38 +1,12 @@
 package web;
 
-import org.springframework.ui.Model;
-import java.math.BigDecimal;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import database.SessionManager;
-import model.Account;
-import java.util.Optional;
-import java.util.logging.Logger;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.servlet.ModelAndView;
-import database.SessionManager;
 @Controller
 public class CreateAccountController {
 
